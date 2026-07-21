55552bd127a0fd92c2557da4babacf9681585e16
9852917330 <superdaddy8691@gmail.com>
2026-07-21 22:43:24 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,63 +1,28 @@
-const CACHE_NAME = 'millionaire-cache-v17-30coin-rotation';
-const APP_SHELL = [
-  "./",
-  "./index.html",
-  "./millionaire.html",
-  "./manifest.webmanifest",
-  "./millionaire-icon-192-v14.png",
-  "./millionaire-icon-512-v14.png",
-  "./millionaire-apple-touch-icon-v14.png",
-  "./millionaire-og-image-v14.png",
-  "./millionaire-thumbnail-v14.png",
-  "./millionaire-favicon-v14.ico",
-  "./favicon.ico",
-  "./icon-192.png",
-  "./icon-512.png",
-  "./apple-touch-icon.png",
-  "./og-image.png",
-  "./thumbnail.png"
+const CACHE_NAME="millionaire-v19-30coin-tradingview-chart";
+const APP_SHELL=[
+  "./","./index.html","./millionaire.html","./manifest.webmanifest",
+  "./millionaire-favicon-v14.ico","./millionaire-icon-192-v14.png","./millionaire-icon-512-v14.png",
+  "./millionaire-apple-touch-icon-v14.png"
 ];
-
-self.addEventListener("install", event => {
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
 });
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
-      .then(() => self.clients.claim())
-  );
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME&&/millionaire|crypto100|fullcapital/i.test(key)).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
 });
-
-self.addEventListener("fetch", event => {
-  const request = event.request;
-  if (request.method !== "GET") return;
-  const url = new URL(request.url);
-  if (url.origin !== self.location.origin) return;
-  if (request.mode === "navigate") {
-    event.respondWith(
-      fetch(request, { cache: "no-store" })
-        .then(response => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
-          return response;
-        })
-        .catch(() => caches.match(request).then(match => match || caches.match("./millionaire.html") || caches.match("./index.html")))
-    );
+self.addEventListener("fetch",event=>{
+  const request=event.request;
+  if(request.method!=="GET")return;
+  const url=new URL(request.url);
+  if(url.origin!==self.location.origin)return;
+  const isDocument=request.mode==="navigate"||request.destination==="document"||/\.(?:html?|webmanifest)$/i.test(url.pathname);
+  if(isDocument){
+    event.respondWith(fetch(request).then(response=>{
+      const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(request,copy));return response;
+    }).catch(()=>caches.match(request).then(hit=>hit||caches.match("./millionaire.html"))));
     return;
   }
-  event.respondWith(
-    caches.match(request).then(cached => {
-      const network = fetch(request).then(response => {
-        if (response && response.ok) {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
-        }
-        return response;
-      }).catch(() => cached);
-      return cached || network;
-    })
-  );
+  event.respondWith(caches.match(request).then(hit=>hit||fetch(request).then(response=>{
+    const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(request,copy));return response;
+  })));
 });
