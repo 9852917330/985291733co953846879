54644a3d031076d195b60e62268c3d06d403613c
9852917330 <superdaddy8691@gmail.com>
2026-07-24 22:56:00 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="millionaire-v24-rsi-band-no-grid-large-chart-button";
+const CACHE_NAME="millionaire-v25-full-trade-history-quantity-rebuild";
 const APP_SHELL=[
   "./","./index.html","./millionaire.html","./manifest.webmanifest",
   "./millionaire-favicon-v14.ico","./millionaire-icon-192-v14.png","./millionaire-icon-512-v14.png",
