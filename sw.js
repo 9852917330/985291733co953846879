c13d6956ca0755829e670aa1137bfdc98189a4c2
9852917330 <superdaddy8691@gmail.com>
2026-07-31 08:47:22 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="millionaire-v27-rebuy-signal-under-btc";
+const CACHE_NAME="millionaire-v28-add-zksync-zk";
 const APP_SHELL=[
   "./","./index.html","./millionaire.html","./manifest.webmanifest",
   "./millionaire-favicon-v14.ico","./millionaire-icon-192-v14.png","./millionaire-icon-512-v14.png",
