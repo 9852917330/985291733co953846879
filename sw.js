674467b31a0c776474d3a9b4a264bdf03c71e748
9852917330 <superdaddy8691@gmail.com>
2026-08-03 01:21:19 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="millionaire-v32-25coin-three-quantity-milestones";
+const CACHE_NAME="millionaire-v33-overview-three-milestone-grid";
 const APP_SHELL=[
   "./","./index.html","./millionaire.html","./manifest.webmanifest",
   "./millionaire-favicon-v14.ico","./millionaire-icon-192-v14.png","./millionaire-icon-512-v14.png",
