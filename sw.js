5e02c07b38947d2a2307b967a8d8ab34ba4a1039
9852917330 <superdaddy8691@gmail.com>
2026-09-13 08:51:47 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="millionaire-v47-24coin-ena-exact-header";
+const CACHE_NAME="millionaire-v48-20coin-hype-eth";
 const APP_SHELL=[
   "./","./index.html","./millionaire.html","./manifest.webmanifest",
   "./millionaire-favicon-v14.ico","./millionaire-icon-192-v14.png","./millionaire-icon-512-v14.png",
